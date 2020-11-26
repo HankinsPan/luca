4c9a6faa7a7ef9552a4c629fe604737e007f8988
HankinsPan <hankins.home@outlook.com>
2020-11-26 15:17:40 +0800

    modify html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let onSearch = function () {
+  let keyWords = document.getElementsByClassName('text')
+  let search_result = document.getElementsByClassName('search_result')
+
+  console.log('==== keyWords >>>>', keyWords)
+  console.log('==== search_result >>>>', search_result)
+
+}
