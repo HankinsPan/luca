b90482c249716ecd9790d2784ab56555b21dfc14
HankinsPan <hankins.home@outlook.com>
2021-01-18 15:32:50 +0800

    add logo png

--- src/Home/data.source.js
+++ src/Home/data.source.js
@@ -103,8 +103,7 @@ export const Banner00DataSource = {
   textWrapper: { className: 'banner0-text-wrapper' },
   title: {
     className: 'banner0-title',
-    // children: 'https://zos.alipayobjects.com/rmsportal/HqnZZjBjWRbjyMr.png',
-    children: 'https://github.com/HankinsPan/luca/blob/master/src/assets/draw/JSBridge_03.svg',
+    children: 'https://raw.githubusercontent.com/HankinsPan/luca/master/src/assets/draw/JSBridge_03.svg',
   },
   content: {
     className: 'banner0-content',
@@ -118,7 +117,7 @@ export const Content00DataSource = {
   OverPack: { playScale: 0.3, className: '' },
   titleWrapper: {
     className: 'title-wrapper',
-    children: [{ name: 'title', children: '产品与服务' }],
+    children: [{ name: 'title', children: '集成目标' }],
   },
   childWrapper: {
     className: 'content0-block-wrapper',
@@ -140,9 +139,9 @@ export const Content00DataSource = {
             {
               name: 'title',
               className: 'content0-block-title',
-              children: '一站式业务接入',
+              children: 'ReactNative与H5双向通讯',
             },
-            { name: 'content', children: '支付、结算、核算接入产品效率翻四倍' },
+            { name: 'content', children: '双向通讯是Bridge的基础' },
           ],
         },
       },
@@ -163,11 +162,11 @@ export const Content00DataSource = {
             {
               name: 'title',
               className: 'content0-block-title',
-              children: '一站式事中风险监控',
+              children: 'Bridge黑盒操作',
             },
             {
               name: 'content',
-              children: '在所有需求配置环节事前风险控制和质量控制能力',
+              children: '隐藏数据通讯细节，只暴露通讯接口',
             },
           ],
         },
@@ -189,11 +188,11 @@ export const Content00DataSource = {
             {
               name: 'title',
               className: 'content0-block-title',
-              children: '一站式数据运营',
+              children: '插件化',
             },
             {
               name: 'content',
-              children: '沉淀产品接入效率和运营小二工作效率数据',
+              children: '可扩展性、易用性、兼容性',
             },
           ],
         },
@@ -209,11 +208,11 @@ export const Content10DataSource = {
     children: 'https://zos.alipayobjects.com/rmsportal/nLzbeGQLPyBJoli.png',
   },
   textWrapper: { className: 'content1-text', md: 14, xs: 24 },
-  title: { className: 'content1-title', children: '企业资源管理' },
+  title: { className: 'content1-title', children: '插件相关依赖' },
   content: {
     className: 'content1-content',
     children:
-      '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。',
+      'react-native@0.63.1 \nreact-native-web-view@11.0.3 \nreact@16.13.1',
   },
 }
 export const Content30DataSource = {
@@ -225,13 +224,13 @@ export const Content30DataSource = {
     children: [
       {
         name: 'title',
-        children: '蚂蚁金融云提供专业的服务',
+        children: 'newBanker-Alpha EE项目',
         className: 'title-h1',
       },
       {
         name: 'content',
         className: 'title-content',
-        children: '基于阿里云强大的基础资源',
+        children: '移动端内部交付项目',
       },
     ],
   },
@@ -250,11 +249,11 @@ export const Content30DataSource = {
               'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
           },
           textWrapper: { className: 'content3-text' },
-          title: { className: 'content3-title', children: '企业资源管理' },
+          title: { className: 'content3-title', children: '社交分享' },
           content: {
             className: 'content3-content',
             children:
-              '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。',
+              '微信（朋友圈 、好友）',
           },
         },
       },
@@ -270,11 +269,11 @@ export const Content30DataSource = {
               'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
           },
           textWrapper: { className: 'content3-text' },
-          title: { className: 'content3-title', children: '云安全' },
+          title: { className: 'content3-title', children: '网络请求' },
           content: {
             className: 'content3-content',
             children:
-              '按金融企业安全要求打造的完整云上安全体系，全方位保障金融应用及数据安全。',
+              'http请求、上传、下载、本地存储',
           },
         },
       },
@@ -290,11 +289,11 @@ export const Content30DataSource = {
               'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
           },
           textWrapper: { className: 'content3-text' },
-          title: { className: 'content3-title', children: '云监控' },
+          title: { className: 'content3-title', children: '文件阅览' },
           content: {
             className: 'content3-content',
             children:
-              '分布式云环境集中监控，统一资源及应用状态视图，智能分析及故障定位。',
+              'pdf预览',
           },
         },
       },
@@ -310,11 +309,11 @@ export const Content30DataSource = {
               'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
           },
           textWrapper: { className: 'content3-text' },
-          title: { className: 'content3-title', children: '移动' },
+          title: { className: 'content3-title', children: '媒体播放' },
           content: {
             className: 'content3-content',
             children:
-              '一站式移动金融APP开发及全面监控；丰富可用组件，动态发布和故障热修复。',
+              'MP4视频播放',
           },
         },
       },
@@ -330,11 +329,11 @@ export const Content30DataSource = {
               'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
           },
           textWrapper: { className: 'content3-text' },
-          title: { className: 'content3-title', children: '分布式中间件' },
+          title: { className: 'content3-title', children: '文本处理' },
           content: {
             className: 'content3-content',
             children:
-              '金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。',
+              '文本复制',
           },
         },
       },
@@ -350,11 +349,51 @@ export const Content30DataSource = {
               'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
           },
           textWrapper: { className: 'content3-text' },
-          title: { className: 'content3-title', children: '大数据' },
+          title: { className: 'content3-title', children: '设备信息' },
           content: {
             className: 'content3-content',
             children:
-              '一站式、全周期大数据协同工作平台，PB级数据处理、毫秒级数据分析工具。',
+              '监听app前后台状态、监听横竖屏状态',
+          },
+        },
+      },
+      {
+        name: 'block6',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '提示窗' },
+          content: {
+            className: 'content3-content',
+            children:
+              '警告、模态窗',
+          },
+        },
+      },
+      {
+        name: 'block7',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '路由' },
+          content: {
+            className: 'content3-content',
+            children:
+              '跳转、关闭/返回、跳转到对应的tab页',
           },
         },
       },
@@ -368,7 +407,7 @@ export const Footer00DataSource = {
     className: 'copyright',
     children: (
       <span>
-        ©2018 <a href="https://motion.ant.design">Ant Motion</a> All Rights
+        ©2021 <a href="https://motion.ant.design">newBanker Alpha</a> All Rights
         Reserved
       </span>
     ),
